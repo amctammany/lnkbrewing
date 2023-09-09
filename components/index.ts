c72a70f4e9a7cc6b962707bb836190431b1afdeb
Alex McTammany <apmctammany@gmail.com>
2023-09-09 00:07:01 -0400

    cleanup

--- components/index.ts
+++ components/index.ts
@@ -3,7 +3,4 @@ export * from "./Form";
 export * from "./List";
 export * from "./Table";
 export * from "./Nav";
-export * from "./HopSelect";
-export * from "./FermentableSelect";
 export * from "./Section";
-export * from "./StyleSelect";
