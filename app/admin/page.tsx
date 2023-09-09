45cdd45ba4beffc87f2cb0cf8bae72293bc2f3a4
Alex McTammany <apmctammany@gmail.com>
2023-09-09 09:59:37 -0400

    checkin before prisma adapter

---
+++ app/admin/page.tsx
@@ -0,0 +1,15 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function AdminPage() {
+  const session = await getServerSession(authOptions);
+  if (!session) return redirect("/");
+
+  return (
+    <div>
+      AdminPage
+      <h4>{session?.user?.email}</h4>
+    </div>
+  );
+}
--- app/middleware.ts
+++ app/middleware.ts
@@ -5,7 +5,8 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(function middleware(req) {}, {
   callbacks: {
     authorized: ({ req, token }) => {
-      if (req.nextUrl.pathname.startsWith("/protected") && token === null) {
+      console.log({ req, token });
+      if (req.nextUrl.pathname.startsWith("/admin") && token === null) {
         return false;
       }
       return true;
