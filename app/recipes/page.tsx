9d4fc5782935d9a4308f7fe202a2ca55aca40d8d
Alex McTammany <apmctammany@gmail.com>
2023-09-09 13:09:23 -0400

    added styling to recipesindex

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,4 +1,4 @@
-import { ButtonLink, List, ListItemButton } from "@/components";
+import { ButtonLink, List, ListItemButton, Section } from "@/components";
 import { prisma } from "@/lib/client";
 import { Metadata } from "next";
 import { getServerSession } from "next-auth";
@@ -7,32 +7,34 @@ export const metadata: Metadata = {
   title: "LNK Recipes",
 };
 export default async function RecipesIndex() {
-  const session = await getServerSession(authOptions);
-  console.log(session);
   const recipes = await prisma.recipe.findMany();
   return (
-    <div>
-      <ButtonLink href="/recipes/new">New</ButtonLink>
+    <Section
+      header="RecipesIndex"
+      actions={<ButtonLink href="/recipes/new">New</ButtonLink>}
+    >
       <List>
         {recipes.map((recipe) => (
           <ListItemButton key={recipe.id} href={`/recipes/${recipe.id}`}>
             <div className="flex items-center space-x-4">
               <div className="flex-shrink-0">
                 <div className="px-2.5 py-2.5 rounded-full border w-16 text-center bg-slate-200 text-black group-hover:text-white group-hover:bg-gray-300 text-sm font-medium">
-                  {recipe.authorId}
+                  {recipe.styleIdentifer}
                 </div>
               </div>
 
               <div className="flex-1 min-w-0">
-                <p className="group-hover:text-slate-400"> {recipe.name}</p>
-                <p className="group-hover:text-slate-400">
-                  {recipe.styleIdentifer}
+                <p className="text-md group-hover:text-slate-400">
+                  {recipe.name}
+                </p>
+                <p className="text-sm group-hover:text-slate-400">
+                  {recipe.authorEmail}
                 </p>
               </div>
             </div>
           </ListItemButton>
         ))}
       </List>
-    </div>
+    </Section>
   );
 }
