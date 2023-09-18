64be4d00257ec6dd8c668ea9ff0635c2ebd39571
Alex McTammany <apmctammany@gmail.com>
2023-09-18 13:22:34 -0400

    fuck nav

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { AuthenticationNav } from ".";
+import { Button, Menu } from "..";
 
 export type NavProps = {
   children: React.ReactNode;
@@ -7,27 +8,33 @@ export type NavProps = {
 
 export const Nav = ({ children }: NavProps) => {
   return (
-    <nav className="bg-slate-600 border-gray-200 ">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
-        <div className="flex flex-row items-center flex-grow ">
-          <Link
-            href="/"
-            className="flex-none md:hidden text-slate-200 font-extrabold text-2xl py-2 px-4 mr-4"
-          >
-            LNK
-          </Link>
+    <nav className="flex items-center md:justify-between flex-wrap md:flex-nowrap px-5 md:py-2 bg-slate-700">
+      <div className="md:hidden">
+        <Menu label="Open">
+          <ul className="bg-black mr-10">{children}</ul>
+        </Menu>
+      </div>
 
-          <Link
-            href="/"
-            className="flex-none hidden md:block text-slate-200 font-extrabold text-2xl py-2 px-4 mr-4"
-          >
-            LNK Brewing
-          </Link>
-          <ul className="flex flex-grow items-center flex-row font-medium p-2 md:p-0  md:space-x-8">
-            {children}
-          </ul>
-          <AuthenticationNav />
-        </div>
+      <div className="flex items-center flex-shrink-0 mr-6 lg:mr-16">
+        <Link
+          href="/"
+          className="flex-none md:hidden text-slate-200 font-extrabold text-2xl py-2 px-4 mr-4"
+        >
+          LNK
+        </Link>
+
+        <Link
+          href="/"
+          className="flex-none hidden md:block text-slate-200 font-extrabold text-2xl py-2 px-4 mr-4"
+        >
+          LNK Brewing
+        </Link>
+      </div>
+      <div className="hidden w-full flex-grow md:flex lg:items-center lg:w-auto">
+        <ul className="flex flex-grow items-center flex-row font-medium p-2 md:p-0  md:space-x-8">
+          {children}
+        </ul>
+        <AuthenticationNav />
       </div>
     </nav>
   );
