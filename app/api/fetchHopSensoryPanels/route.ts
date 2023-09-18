3cf498a272028a45f60d23a170909300ec617d24
Alex McTammany <apmctammany@gmail.com>
2023-09-18 11:43:49 -0400

    added hopSensoryPanels

---
+++ app/api/fetchHopSensoryPanels/route.ts
@@ -0,0 +1,77 @@
+import * as cheerio from "cheerio";
+import { prisma } from "@/lib/client";
+import { NextResponse } from "next/server";
+import axios from "axios";
+const categories = [
+  "sweetAromatic",
+  "berry",
+  "stoneFruit",
+  "pomme",
+  "melon",
+  "tropical",
+  "citrus",
+  "floral",
+  "herbal",
+  "vegetal",
+  "grassy",
+  "earthy",
+  "woody",
+  "spicy",
+] as const;
+type CategoryName = keyof typeof categories;
+type Point = [number, number];
+const origin: Point = [310, 310];
+const dist = (p1: Point, p2: Point) =>
+  Math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2);
+
+const fetchSensoryData = async (path: string, hopId: number) =>
+  new Promise((resolve, reject) => {
+    axios(path)
+      .then((res) => {
+        if (!res) return reject();
+        if (res.status == 404) return reject();
+        const $ = cheerio.load(res.data);
+        const pointStr = $(
+          ".sensory-analysis-chart > polygon.polygon-chart"
+        ).attr("points");
+        if (!pointStr) return resolve(null);
+        //if (!pointStr) return reject(new Error("invalid?"));
+        const points = pointStr
+          .split(" ")
+          .map((s) => s.split(",").map((n) => parseFloat(n)));
+        const dists = (points as Point[]).map((p: Point) =>
+          (dist(p, origin) / 310).toPrecision(2)
+        );
+
+        const flavorMap = dists.reduce(
+          (acc, d, i) => {
+            acc[categories[i]] = parseFloat(d);
+            return acc;
+          },
+          { hopId } as Record<any, number>
+        );
+
+        return resolve(flavorMap);
+      })
+      .catch((err) => reject(null));
+  });
+
+function getHopUrl(slug: String) {
+  return `https://www.yakimachief.com/${slug}.html`;
+}
+export async function GET() {
+  const hops = await prisma.hop.findMany({
+    select: { slug: true, id: true },
+  });
+  const res = await Promise.allSettled(
+    hops.map(async ({ slug, id }) => {
+      return fetchSensoryData(getHopUrl(slug), id);
+    })
+  );
+  const data = res
+    .filter((r) => r.status == "fulfilled")
+    .map((f: any) => f.value)
+    .filter((f) => f !== null);
+  await prisma.hopSensoryPanel.createMany({ data });
+  return NextResponse.json(data);
+}
