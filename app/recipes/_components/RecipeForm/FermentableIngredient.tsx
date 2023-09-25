f0ba8905f0d89092fd06eff434954e8dbbe089cc
Alex McTammany <apmctammany@gmail.com>
2023-09-25 02:16:42 -0400

    styled fermentableingredient

--- app/recipes/_components/RecipeForm/FermentableIngredient.tsx
+++ app/recipes/_components/RecipeForm/FermentableIngredient.tsx
@@ -4,6 +4,7 @@ import {
   FermentableIngredient as FermentableIngredientType,
 } from "@prisma/client";
 import { ExtendedFermentableIngredient } from "../../types";
+import Link from "next/link";
 
 export type FermentableIngredientProps = {
   fermentable: ExtendedFermentableIngredient;
@@ -12,17 +13,30 @@ export const FermentableIngredient = ({
   fermentable,
 }: FermentableIngredientProps) => {
   return (
-    <ListItem key={fermentable.id}>
-      <div className="flex gap-4">
-        <div className="flex-1">{fermentable.fermentable.name}</div>
-        <div className="flex-0">{fermentable.amount}</div>
-        <div className="flex-0">{fermentable.amountType}</div>
-        <div>
-          <ButtonLink scroll={false} href={`?fermentableId=${fermentable.id}`}>
-            Edit
-          </ButtonLink>
+    <ListItem>
+      <Link scroll={false} href={`?fermentableId=${fermentable.id}`}>
+        <div className="px-2 pb-1 grid grid-cols-6 gap-2 group-hover:bg-slate-500/10 ">
+          <div className="">
+            <div className="">
+              <div className="text-lg ">{fermentable.usage}</div>
+            </div>
+          </div>
+          <div className="col-span-4">
+            <div className="">
+              <div className="text-lg">{fermentable.fermentable.name}</div>
+              <div className="text-xs">
+                Potential: {fermentable.fermentable.potential}
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="">
+              <div className="text-lg">{fermentable.amount}</div>
+              <div className="text-xs">{fermentable.amountType}</div>
+            </div>
+          </div>
         </div>
-      </div>
+      </Link>
     </ListItem>
   );
 };
