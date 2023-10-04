79b257502f70b0cfd3b6ca7a4330f29745cdd2dc
Alex McTammany <apmctammany@gmail.com>
2023-10-04 01:07:12 -0400

    client fix

--- lib/client.ts
+++ lib/client.ts
@@ -1,10 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaClient as PrismaClientEdge } from "@prisma/client/edge";
+
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  process.env.NODE_ENV === "production"
-    ? new PrismaClientEdge()
-    : globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
