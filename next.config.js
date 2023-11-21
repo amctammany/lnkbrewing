c84c40b1dd8e5a07e0f9ad74732a0c1e869a83a8
Alex McTammany <apmctammany@gmail.com>
2023-11-21 00:30:07 -0500

    upgraded next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  experimental: {},
 };
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
