174317638fa25abab6451023f08d37e01bafd961
Alex McTammany <apmctammany@gmail.com>
2023-11-21 05:29:39 -0500

    added steps to mash profile display

--- app/profiles/mash/[slug]/page.tsx
+++ app/profiles/mash/[slug]/page.tsx
@@ -1,8 +1,12 @@
 import { ButtonLink } from "@/components/Button/Button";
+import { List } from "@/components/List/List";
+import { ListItem } from "@/components/List/ListItem";
 import { Section } from "@/components/Section/Section";
 import { prisma } from "@/lib/client";
-import { MashProfile } from "@prisma/client";
+import { MashProfile, MashStep } from "@prisma/client";
 import Link from "next/link";
+import { MashProfileStep } from "../types";
+import Prop from "@/components/Prop/Prop";
 type MashProfileDisplayProps = {
   params: {
     slug: string;
@@ -16,6 +20,25 @@ export function generateMetadata({ params }: MashProfileDisplayProps) {
     title: `LNK MashProfile: ${params.slug}`,
   };
 }
+type MashStepListItemProps = {
+  step: MashStep;
+  index: number;
+};
+function MashStepListItem({ step, index }: MashStepListItemProps) {
+  return (
+    <ListItem key={step.id}>
+      <div className="grid grid-cols-5">
+        <div>
+          <b>{index}</b>
+        </div>
+        <Prop label="Name" value={step.name} />
+        <Prop label="Temperature" value={step.temperature} unit={"0F"} />
+        <Prop label="Time" value={step.time} unit="min" />
+        <Prop label="Ramp Time" value={step.rampTime} unit="min" />
+      </div>
+    </ListItem>
+  );
+}
 
 export default async function MashProfileDisplay({
   params: { slug },
@@ -24,6 +47,7 @@ export default async function MashProfileDisplay({
     where: {
       slug,
     },
+    include: { steps: true },
   });
   return (
     <Section
@@ -42,7 +66,14 @@ export default async function MashProfileDisplay({
           <p className="px-2 m-2">{mashProfile?.[field]}</p>
         </div>
       ))}
-
+      <div className="m-2 p-2">
+        <h2 className="text-lg uppercase underline">Steps</h2>
+        <List>
+          {(mashProfile?.steps || []).map((step, index) => (
+            <MashStepListItem key={step.id} step={step} index={index} />
+          ))}
+        </List>
+      </div>
       <div className="grid gap-2 md:gap-4 grid-cols-2 md:grid-cols-4">
         {numberFieldNames.map((field) => (
           <div key={field} className="m-2 p-2 bg-white shadow-sm drop-shadow">
