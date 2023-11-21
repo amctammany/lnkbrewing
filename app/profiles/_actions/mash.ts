04a7cd465ec3168b5e8568ecbcc46c0e255ece87
Alex McTammany <apmctammany@gmail.com>
2023-11-21 01:59:14 -0500

    added crud for mash profile

--- app/profiles/_actions/mash.ts
+++ app/profiles/_actions/mash.ts
@@ -1,6 +1,7 @@
 "use server";
 import { prisma } from "@/lib/client";
 import { redirect } from "next/navigation";
+import slugify from "slugify";
 import { z } from "zod";
 import { zfd } from "zod-form-data";
 
@@ -9,12 +10,21 @@ const mashSchema = zfd.formData({
   name: zfd.text(),
   description: zfd.text(z.string().optional()),
 });
-
+export const createMashProfile = async (formData: FormData) => {
+  const data = mashSchema.parse(formData);
+  const res = await prisma.mashProfile.create({
+    data: {
+      ...data,
+      slug: slugify(data.name, { lower: true }),
+    },
+  });
+  redirect(`/profiles/mash/${res.slug}`);
+};
 export const updateMashProfile = async (formData: FormData) => {
   const data = mashSchema.parse(formData);
   const res = await prisma.mashProfile.update({
     where: { id: data.id },
-    data,
+    data: { ...data, slug: slugify(data.name, { lower: true }) },
   });
   redirect(`/profiles/mash/${res.slug}`);
 };
--- app/profiles/_components/MashProfileForm/MashProfileForm.tsx
+++ app/profiles/_components/MashProfileForm/MashProfileForm.tsx
@@ -1,43 +1,74 @@
+"use client";
 import { Form, NumberField, Submit, TextArea, TextField } from "@/components";
 import { MashProfile } from "@prisma/client";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { createMashProfile, updateMashProfile } from "../../_actions";
 
 export type MashProfileFormProps = {
   src: MashProfile | null;
-  action?: (formData: FormData) => void;
+  //action?: (formData: FormData) => void;
 };
-type Diff<T, U> = T extends U ? never : T;
+type MashProfileStep = {
+  name?: string;
+  type: any;
+  temperature: number;
+  time: number;
+  rampTime?: number;
+};
+type MashProfileInput = {
+  id: number;
+  name?: string;
+  description?: string;
+  steps: MashProfileStep[];
+};
+export const MashProfileForm = ({ src }: MashProfileFormProps) => {
+  const { register, handleSubmit, reset, setValue } = useForm<MashProfileInput>(
+    {
+      defaultValues: src || {},
+    }
+  );
+  const action = src?.id ? updateMashProfile : createMashProfile;
 
-type NonNullable<T> = Diff<T, null | undefined>;
+  const onSubmit: SubmitHandler<MashProfileInput> = (data) => {
+    const body = new FormData();
 
-type NumberKeys<T extends object> = {
-  [K in keyof T]: NonNullable<T[K]> extends number ? K : never;
-}[keyof T];
+    Object.entries(data).forEach(([key, value]) => {
+      if (value !== null) {
+        body.append(key, value?.toString());
+      }
+    });
+    console.log(data);
+    action(body);
+  };
 
-const numberFields: NumberKeys<MashProfile>[] = [];
-export const MashProfileForm = ({ src, action }: MashProfileFormProps) => {
+  //const handleChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
+  //const { name, value } = e.currentTarget;
+  //const fermentable = fermentables.find((p) => p.id === parseInt(value));
+  //if (!fermentable) return;
+  //setValue("fermentableId", fermentable?.id);
+  //setValue("potential", fermentable?.potential);
+  //setValue("color", fermentable?.color);
+  //};
+  //const options = (fermentables || []).reduce((acc, hop) => {
+  //acc[hop.id] = hop.name;
+  //return acc;
+  //}, {} as Record<string, string>);
+
   return (
-    <Form action={action}>
-      <h5>Mash Profile {JSON.stringify(src)}</h5>
-      <input type="hidden" name="id" value={src?.id} />
-      <TextField name="name" label="Name" defaultValue={src?.name} />
-      <TextArea
-        name="description"
-        rows={3}
-        label="Description"
-        defaultValue={src?.description}
-      />{" "}
-      <div className="grid gap-4 grid-cols-2 md:grid-cols-4">
-        {numberFields.map((f) => (
-          <NumberField
-            key={f}
-            name={f}
-            label={f}
-            step={0.1}
-            defaultValue={src?.[f]}
-          />
-        ))}
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <div className="grid gap-2 md:gap-4 grid-cols-1 md:grid-cols-2">
+        <input type="hidden" {...register("id")} />
+        <div className="col-span-2">
+          <TextField {...register("name")} label="Name" />
+        </div>
+        <div className="col-span-2">
+          <TextField {...register("description")} label="Description" />
+        </div>
+
+        <div className="col-span-2">
+          <Submit>Save</Submit>
+        </div>
       </div>
-      <Submit>Save</Submit>
     </Form>
   );
 };
