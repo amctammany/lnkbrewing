687f3337a6e41f41740eec98733412d8f6f4b86b
Alex McTammany <apmctammany@gmail.com>
2023-11-25 04:07:05 -0500

    section uses cva

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -1,22 +1,76 @@
-export type SectionProps = {
+import { VariantProps, cva } from "class-variance-authority";
+
+const section = cva(["flex"], {
+  variants: {
+    variant: {
+      primary: ["bg-slate-200"],
+    },
+    size: {
+      small: ["te"],
+      default: ["min-w-full"],
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "default",
+  },
+});
+
+const sectionHeader = cva("h4", {
+  variants: {
+    variant: {
+      primary: ["border-red-300"],
+    },
+    size: {
+      small: ["flex-grow", "m-0", "py-1", "px-2", "text-sm"],
+      default: ["flex-grow", "m-1", "py-2", "px-4", "text-lg", "font-bold"],
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "default",
+  },
+});
+const sectionBody = cva(["bg-white"], {
+  variants: {
+    variant: {
+      primary: ["border-red-300"],
+    },
+    size: {
+      small: ["p-1", "shadow-sm", "text-sm"],
+      default: ["p-2", "shadow-lg", "text-lg"],
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "default",
+  },
+});
+
+export interface SectionProps extends VariantProps<typeof section> {
   header?: string;
   icon?: string;
   actions?: React.ReactNode | React.ReactNode[];
   children?: React.ReactNode | React.ReactNode[];
-};
+}
 
-export const Section = ({ header, icon, actions, children }: SectionProps) => {
+export const Section = ({
+  header,
+  icon,
+  actions,
+  children,
+  size,
+  variant,
+}: SectionProps) => {
   return (
     <div className="min-w-full ">
-      <div className="flex bg-slate-200 ">
+      <div className={section({ size, variant })}>
         <div className="flex-shrink">{icon}</div>
-        <h4 className="flex-grow text-lg font-bold py-2 px-4 m-0 border-red-300">
-          {header}
-        </h4>
+        <h4 className={sectionHeader({ size, variant })}>{header}</h4>
         <div>{actions}</div>
       </div>
 
-      <div className="bg-white p-2  shadow-lg">{children}</div>
+      <div className={sectionBody({ size, variant })}>{children}</div>
     </div>
   );
 };
