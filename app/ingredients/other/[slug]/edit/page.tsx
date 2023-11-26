7b40d1d6835ef42eb209eceb27f344b3838ff675
Alex McTammany <apmctammany@gmail.com>
2023-11-26 00:27:44 -0500

    checkin

--- app/ingredients/other/[slug]/edit/page.tsx
+++ app/ingredients/other/[slug]/edit/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/client";
 import { OtherIngredientForm } from "../../_components";
 import { updateOtherIngredient } from "../../actions";
+import { getOtherIngredientOptions } from "../../queries";
 type OtherIngredientDisplayProps = {
   params: {
     slug: string;
@@ -21,11 +22,13 @@ export default async function OtherIngredientDisplay({
       slug,
     },
   });
+  const options = await getOtherIngredientOptions();
   return (
     <div className="m-5 p-0 min-w-full bg-slate-200">
       <OtherIngredientForm
         src={otherIngredient}
         action={updateOtherIngredient}
+        options={options}
       />
     </div>
   );
--- app/ingredients/other/_components/OtherIngredientForm/OtherIngredientForm.tsx
+++ app/ingredients/other/_components/OtherIngredientForm/OtherIngredientForm.tsx
@@ -1,24 +1,26 @@
 "use client";
-import { Form, NumberField, Submit, TextArea, TextField } from "@/components";
-import { OtherIngredient } from "@prisma/client";
+import {
+  Select,
+  Form,
+  NumberField,
+  Submit,
+  TextArea,
+  TextField,
+} from "@/components/Form";
+import { NumberKeys } from "@/lib/types";
+import { OtherIngredient, OtherIngredientType } from "@prisma/client";
 import { useForm } from "react-hook-form";
 
 export type OtherIngredientFormProps = {
   src: OtherIngredient | null;
   action: (formData: FormData) => void;
+  options: any;
 };
-type Diff<T, U> = T extends U ? never : T;
-
-type NonNullable<T> = Diff<T, null | undefined>;
-
-type NumberKeys<T extends object> = {
-  [K in keyof T]: NonNullable<T[K]> extends number ? K : never;
-}[keyof T];
-
 const numberFields: NumberKeys<OtherIngredient>[] = [];
 export const OtherIngredientForm = ({
   src,
   action,
+  options,
 }: OtherIngredientFormProps) => {
   const { control, register, trigger } = useForm<OtherIngredient>({
     defaultValues: src || {},
@@ -36,6 +38,11 @@ export const OtherIngredientForm = ({
       <TextField label="Name" {...register("name")} />
       <TextArea rows={3} label="Description" {...register("description")} />
 
+      <Select
+        label="Other"
+        {...register("type")}
+        options={OtherIngredientType}
+      />
       <div className="grid gap-4 grid-cols-2 md:grid-cols-4">
         {numberFields.map((f) => (
           <NumberField key={f} label={f} {...register(f)} />
