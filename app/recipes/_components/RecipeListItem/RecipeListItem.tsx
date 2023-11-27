0256f40a6caf4b86335d7cd4e012d40ef8f39df3
Alex McTammany <apmctammany@gmail.com>
2023-11-27 14:18:15 -0500

    recipe list item update

---
+++ app/recipes/_components/RecipeListItem/RecipeListItem.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { ListItem } from "@/components/List";
+import { removeRecipe } from "../../actions";
+import { TrashIcon } from "@heroicons/react/24/solid";
+import { Recipe } from "@prisma/client";
+type RemoveRecipeButtonProps = {
+  id?: number;
+};
+const RemoveRecipeButton = ({ id }: RemoveRecipeButtonProps) => {
+  return (
+    <form action={removeRecipe}>
+      <input type="hidden" name="id" value={id} />
+      <button type="submit" className="border-red-300 border rounded-md p-2">
+        <TrashIcon className="h-6 w-6 text-red-500 " />
+      </button>
+    </form>
+  );
+};
+
+type RecipeListItemProps = {
+  recipe?: Partial<Recipe>;
+};
+export const RecipeListItem = ({ recipe }: RecipeListItemProps) => (
+  <ListItem key={recipe?.id}>
+    <div className="flex items-center space-x-4">
+      <Link
+        className="flex-grow px-2 pb-1 grid grid-cols-6 gap-2  "
+        href={`/recipes/${recipe?.id}`}
+      >
+        <div className="flex items-center space-x-4">
+          <div className="flex-shrink-0">
+            <div className="px-2.5 py-2.5 rounded-full border w-16 text-center bg-slate-200 text-black group-hover:text-white group-hover:bg-gray-300 text-sm font-medium">
+              {recipe?.styleIdentifer}
+            </div>
+          </div>
+
+          <div className="flex-1 min-w-0">
+            <p className="text-md group-hover:text-slate-400">{recipe?.name}</p>
+            <p className="text-sm group-hover:text-slate-400">
+              {recipe?.authorEmail}
+            </p>
+          </div>
+        </div>
+      </Link>
+      <div className="m-auto">
+        <RemoveRecipeButton id={recipe?.id} />
+      </div>
+    </div>
+  </ListItem>
+);
--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -13,21 +13,9 @@ import { getRecipes } from "./queries";
 import { removeRecipe } from "./actions";
 import { TrashIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import { RecipeListItem } from "./_components/RecipeListItem/RecipeListItem";
 export const metadata: Metadata = {
   title: "LNK Recipes",
-};
-type RemoveRecipeButtonProps = {
-  id: number;
-};
-const RemoveRecipeButton = ({ id }: RemoveRecipeButtonProps) => {
-  return (
-    <form action={removeRecipe}>
-      <input type="hidden" name="id" value={id} />
-      <button type="submit" className="border-red-300 border rounded-md p-2">
-        <TrashIcon className="h-6 w-6 text-red-500 " />
-      </button>
-    </form>
-  );
 };
 
 export default async function RecipesIndex() {
@@ -39,34 +27,7 @@ export default async function RecipesIndex() {
     >
       <List>
         {recipes.map((recipe) => (
-          <ListItem key={recipe.id}>
-            <div className="flex items-center space-x-4">
-              <Link
-                className="flex-grow px-2 pb-1 grid grid-cols-6 gap-2  "
-                href={`/recipes/${recipe.id}`}
-              >
-                <div className="flex items-center space-x-4">
-                  <div className="flex-shrink-0">
-                    <div className="px-2.5 py-2.5 rounded-full border w-16 text-center bg-slate-200 text-black group-hover:text-white group-hover:bg-gray-300 text-sm font-medium">
-                      {recipe.styleIdentifer}
-                    </div>
-                  </div>
-
-                  <div className="flex-1 min-w-0">
-                    <p className="text-md group-hover:text-slate-400">
-                      {recipe.name}
-                    </p>
-                    <p className="text-sm group-hover:text-slate-400">
-                      {recipe.authorEmail}
-                    </p>
-                  </div>
-                </div>
-              </Link>
-              <div className="m-auto">
-                <RemoveRecipeButton id={recipe.id} />
-              </div>
-            </div>
-          </ListItem>
+          <RecipeListItem key={recipe.id} recipe={recipe} />
         ))}
       </List>
     </Section>
