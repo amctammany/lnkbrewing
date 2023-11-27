34c0c1725c273ee97700b9b8512665ad878a7a4f
Alex McTammany <apmctammany@gmail.com>
2023-11-27 17:18:20 -0500

    added confirm onSubmit to remove btn

--- components/RemoveButton/RemoveButton.tsx
+++ components/RemoveButton/RemoveButton.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { TrashIcon } from "@heroicons/react/24/solid";
 
 export type RemoveButtonProps = {
@@ -6,7 +7,7 @@ export type RemoveButtonProps = {
 };
 export const RemoveButton = ({ id, action }: RemoveButtonProps) => {
   return (
-    <form action={action}>
+    <form action={action} onSubmit={() => confirm("Are you sure?")}>
       <input type="hidden" name="id" value={id} />
       <button type="submit" className="border-red-300 border rounded-md p-2">
         <TrashIcon className="h-6 w-6 text-red-500 " />
