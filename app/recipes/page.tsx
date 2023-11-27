228548bf27062d2660661b04061bb08a049b1aaa
Alex McTammany <apmctammany@gmail.com>
2023-11-27 13:42:47 -0500

    cleanup

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -40,26 +40,31 @@ export default async function RecipesIndex() {
       <List>
         {recipes.map((recipe) => (
           <ListItem key={recipe.id}>
-            <Link href={`/recipes/${recipe.id}`}>
-              <div className="flex items-center space-x-4">
-                <div className="flex-shrink-0">
-                  <div className="px-2.5 py-2.5 rounded-full border w-16 text-center bg-slate-200 text-black group-hover:text-white group-hover:bg-gray-300 text-sm font-medium">
-                    {recipe.styleIdentifer}
+            <div className="flex items-center space-x-4">
+              <Link
+                className="flex-grow px-2 pb-1 grid grid-cols-6 gap-2  "
+                href={`/recipes/${recipe.id}`}
+              >
+                <div className="flex items-center space-x-4">
+                  <div className="flex-shrink-0">
+                    <div className="px-2.5 py-2.5 rounded-full border w-16 text-center bg-slate-200 text-black group-hover:text-white group-hover:bg-gray-300 text-sm font-medium">
+                      {recipe.styleIdentifer}
+                    </div>
+                  </div>
+
+                  <div className="flex-1 min-w-0">
+                    <p className="text-md group-hover:text-slate-400">
+                      {recipe.name}
+                    </p>
+                    <p className="text-sm group-hover:text-slate-400">
+                      {recipe.authorEmail}
+                    </p>
                   </div>
                 </div>
-
-                <div className="flex-1 min-w-0">
-                  <p className="text-md group-hover:text-slate-400">
-                    {recipe.name}
-                  </p>
-                  <p className="text-sm group-hover:text-slate-400">
-                    {recipe.authorEmail}
-                  </p>
-                </div>
+              </Link>
+              <div className="m-auto">
+                <RemoveRecipeButton id={recipe.id} />
               </div>
-            </Link>
-            <div className="m-auto">
-              <RemoveRecipeButton id={recipe.id} />
             </div>
           </ListItem>
         ))}
