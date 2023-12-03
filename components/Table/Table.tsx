b10f036da36823d272d5cf402a4cc71f95febe8a
Alex McTammany <apmctammany@gmail.com>
2023-12-03 12:08:23 -0500

    added direction to table

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import * as D from "@heroicons/react/20/solid";
 import { TableRow, makeTableRow } from "./TableRow";
 import Link from "next/link";
 export type TableProps<T extends Record<string, any>> = {
@@ -20,7 +21,9 @@ export function Table<T extends Record<string, any>>({
 }: TableProps<T>) {
   const Row = makeTableRow(columns);
   const filtered = sort
-    ? src.sort((a, b) => (a[sort] < b[sort] ? -1 : 1))
+    ? src.sort(
+        (a, b) => (a[sort] < b[sort] ? -1 : 1) * (direction === "ASC" ? -1 : 1)
+      )
     : src;
   return (
     <div className="max-w-full overflow-auto">
@@ -29,11 +32,22 @@ export function Table<T extends Record<string, any>>({
           <tr>
             {columns.map(({ name, label }) => (
               <th key={name} className="border border-slate-400">
-                <Link href={`?sort=${name}`}>
-                  <>
-                    <b>{label || name}</b>
-                    <i>{sort === name && <b> selected</b>}</i>
-                  </>
+                <Link
+                  href={`?sort=${name}&direction=${
+                    direction === "ASC" ? "DESC" : "ASC"
+                  }`}
+                >
+                  <div className="flex">
+                    <b className="flex-grow">{label || name}</b>
+                    <i>
+                      {sort === name &&
+                        (direction === "ASC" ? (
+                          <D.ArrowUpIcon className="h-6 w-6" />
+                        ) : (
+                          <D.ArrowDownIcon className="h-6 w-6" />
+                        ))}
+                    </i>
+                  </div>
                 </Link>
               </th>
             ))}
