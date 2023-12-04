d5c0471e8523fea260dc0d56e050a8ab3b40a80e
Alex McTammany <apmctammany@gmail.com>
2023-12-04 11:58:29 -0500

    styling

--- components/List/ListItem.tsx
+++ components/List/ListItem.tsx
@@ -46,7 +46,7 @@ export const ListItem = ({
   return (
     <li className="group relative flex flex-col">
       {child}
-      <div className="z-10 absolute right-2 top-1/2 -translate-y-1/2">
+      <div className="z-10 absolute right-1 top-1/2 -translate-y-1/2">
         {secondaryAction}
       </div>
     </li>
--- components/RemoveButton/RemoveButton.tsx
+++ components/RemoveButton/RemoveButton.tsx
@@ -8,8 +8,11 @@ export const RemoveButton = ({ id, action }: RemoveButtonProps) => {
   return (
     <form action={action}>
       <input type="hidden" name="id" value={id} />
-      <button type="submit" className="border-red-300 border rounded-md p-2">
-        <TrashIcon className="h-6 w-6 text-red-500 " />
+      <button
+        type="submit"
+        className="border-red-300 border hover:text-red-300  hover:bg-white bg-red-300 text-white rounded-md p-2"
+      >
+        <TrashIcon className="h-5 w-5 " />
       </button>
     </form>
   );
