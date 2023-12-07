dd3882364a88e0c69e473e5db8ecdc08ecf5efb6
Alex McTammany <apmctammany@gmail.com>
2023-12-07 01:03:23 -0500

    clean up

--- app/recipes/_components/RecEditor/HopIngredientSection/HopIngredientSectionActions.tsx
+++ app/recipes/_components/RecEditor/HopIngredientSection/HopIngredientSectionActions.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Button, { ButtonLink } from "@/components/Button/Button";
 import React from "react";
-import { AddIcon } from "@/components/Icon";
+import { AddIcon } from "@/components/Icon/AddIcon";
 import { useRecipe } from "../useRecipe";
 
 export const HopIngredientSectionActions = () => {
