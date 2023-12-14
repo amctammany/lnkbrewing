149025759fb40fd61bae015709a958c67067dd73
Alex McTammany <apmctammany@gmail.com>
2023-12-13 22:56:16 -0500

    load

--- app/recipes/[id]/edit/loading.tsx
+++ app/recipes/[id]/edit/loading.tsx
@@ -1,2 +1,11 @@
-export const Loading = ({ id }: { id?: number }) => <div>Loading</div>;
+import { ExtendedRecipe } from "../../types";
+import { Editor } from "./Editor";
+
+export const Loading = ({ id }: { id?: number }) => (
+  <div>
+    Loading
+    <Editor id={id ?? -1} />
+  </div>
+);
+
 export default Loading;
