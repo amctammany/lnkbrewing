856dc6fca76e61aee94a84b2199102079f258040
Alex McTammany <apmctammany@gmail.com>
2023-12-14 23:03:35 -0500

    fixed admin settings form

--- app/admin/settings/AdminForm.tsx
+++ app/admin/settings/AdminForm.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { Form } from "@/components/Form/Form";
 import { Submit } from "@/components/Form/Submit";
 import { TextField } from "@/components/Form/TextField";
 import { User } from "@prisma/client";
 import React, { FC } from "react";
+import { useForm } from "react-hook-form";
 
 interface AdminFormProps {
   user: (User & { recipes: any }) | null;
@@ -10,11 +12,12 @@ interface AdminFormProps {
 }
 
 export const AdminForm: FC<AdminFormProps> = ({ user, action }) => {
+  const { register } = useForm<User>({ defaultValues: user! });
   return (
     <Form action={action}>
-      <input type="hidden" name="id" value={user?.id} />
-      <TextField disabled name="email" defaultValue={user?.email} />
-      <TextField name="name" defaultValue={user?.name} />
+      <input type="hidden" {...register("id")} />
+      <TextField disabled {...register("email")} />
+      <TextField {...register("name")} />
       <Submit>Save</Submit>
     </Form>
   );
