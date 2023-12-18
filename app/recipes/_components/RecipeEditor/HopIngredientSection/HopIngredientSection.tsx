257c35c50953ef494da7ff1b921dbef9f2297b36
Alex McTammany <apmctammany@gmail.com>
2023-12-18 03:36:44 -0500

    added sorting to recipe hop ings

--- app/recipes/_components/RecipeEditor/HopIngredientSection/HopIngredientSection.tsx
+++ app/recipes/_components/RecipeEditor/HopIngredientSection/HopIngredientSection.tsx
@@ -5,14 +5,21 @@ import { HopIngredientListItem } from "./HopIngredientListItem";
 import { HopIngredientSectionActions } from "./HopIngredientSectionActions";
 import { ExtendedRecipe } from "@/app/recipes/types";
 import { HopIcon } from "@/components/Icon/HopIcon";
+import { HopIngredientUsage } from "@prisma/client";
 
 interface HopIngredientSectionProps {
   recipe?: ExtendedRecipe | null;
 }
 
+const usageRank: HopIngredientUsage[] = ["Mash", "Boil", "Whirlpool", "DryHop"];
 export const HopIngredientSection: FC<HopIngredientSectionProps> = ({
   recipe,
 }) => {
+  const sorted = (recipe?.hops || []).sort(
+    (a, b) =>
+      usageRank.findIndex((s) => s === a.usage) -
+        usageRank.findIndex((s) => s === b.usage) || b.duration - a.duration
+  );
   return (
     <Section
       className="md:col-span-2"
@@ -21,7 +28,7 @@ export const HopIngredientSection: FC<HopIngredientSectionProps> = ({
       actions={<HopIngredientSectionActions />}
     >
       <List>
-        {(recipe?.hops || []).map((hop) => (
+        {sorted.map((hop) => (
           <HopIngredientListItem key={hop.id} hop={hop} />
         ))}
       </List>
