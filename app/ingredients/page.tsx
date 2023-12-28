98bd3abef20528f478cc7609566d7dde3ce42909
Alex McTammany <apmctammany@gmail.com>
2023-12-28 00:30:03 -0500

    added icons to ingredientIndex

--- app/ingredients/page.tsx
+++ app/ingredients/page.tsx
@@ -1,21 +1,30 @@
+import { GrainIcon } from "@/components/Icon/GrainIcon";
+import { HopIcon } from "@/components/Icon/HopIcon";
+import { YeastIcon } from "@/components/Icon/YeastIcon";
 import { Section } from "@/components/Section";
 import Link from "next/link";
 
-const links = [
-  ["Hops", "/ingredients/hops"],
-  ["Fermentables", "/ingredients/fermentables"],
-  ["Yeasts", "/ingredients/yeasts"],
-  ["Other", "/ingredients/other"],
+const links: [string, string, typeof HopIcon][] = [
+  ["Hops", "/ingredients/hops", HopIcon],
+  ["Fermentables", "/ingredients/fermentables", GrainIcon],
+  ["Yeasts", "/ingredients/yeasts", YeastIcon],
+  ["Other", "/ingredients/other", HopIcon],
 ];
+export const metadata = {
+  title: "LNK: Ingredients",
+};
 export default function IngredientsIndex() {
   return (
     <Section header="IngredientsIndex">
-      <div className="flex flex-col">
-        {links.map(([label, url]) => (
-          <Link key={url} href={url}>
-            <div className="m-5 p-5 text-center rounded-lg bg-slate-400 border-2 text-white underline uppercase text-lg font-bold ">
-              {label}
-            </div>
+      <div className="grid md:grid-cols-2">
+        {links.map(([label, url, Icon]) => (
+          <Link
+            key={url}
+            href={url}
+            className="m-2 p-5 text-center rounded-lg bg-slate-400 border-2 text-white underline uppercase text-lg font-bold inline-flex"
+          >
+            {Icon && <Icon size="default" className="flex-shrink" />}
+            <span className="flex-grow">{label}</span>
           </Link>
         ))}
       </div>
