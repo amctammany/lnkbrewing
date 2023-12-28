5d98d7073fb7764d8a4bbd17b7e75e5220390264
Alex McTammany <apmctammany@gmail.com>
2023-12-28 13:13:58 -0500

    update profilepage

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -1,20 +1,29 @@
+import {
+  EquipmentProfileIcon,
+  Icon,
+  MashProfileIcon,
+  WaterProfileIcon,
+} from "@/components/Icon";
 import { Section } from "@/components/Section";
 import Link from "next/link";
 
-const links = [
-  ["Equipment", "/profiles/equipment"],
-  ["Mash", "/profiles/mash"],
-  ["Water", "/profiles/water"],
+const links: [string, string, typeof Icon][] = [
+  ["Equipment", "/profiles/equipment", EquipmentProfileIcon],
+  ["Mash", "/profiles/mash", MashProfileIcon],
+  ["Water", "/profiles/water", WaterProfileIcon],
 ];
-export default function IngredientsIndex() {
+export default function ProfilesIndex() {
   return (
     <Section header="Profiles Index">
-      <div className="flex flex-col">
-        {links.map(([label, url]) => (
-          <Link key={url} href={url}>
-            <div className="m-5 p-5 text-center rounded-lg bg-slate-400 border-2 text-white underline uppercase text-lg font-bold ">
-              {label}
-            </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-4">
+        {links.map(([label, url, Icon]) => (
+          <Link
+            key={url}
+            href={url}
+            className="m-5 p-5 text-center rounded-lg bg-slate-400 border-2 text-black underline uppercase text-lg font-bold inline-flex hover:bg-slate-600 hover:text-white"
+          >
+            {Icon && <Icon size="default" className="flex-shrink" />}
+            <span className="flex-grow">{label}</span>
           </Link>
         ))}
       </div>
