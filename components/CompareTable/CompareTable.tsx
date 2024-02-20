dee96e33f171c4e6fa0151d8d4fdd5c777de2099
Alex McTammany <apmctammany@gmail.com>
2024-02-20 11:57:48 -0500

    styling

--- components/CompareTable/CompareTable.tsx
+++ components/CompareTable/CompareTable.tsx
@@ -12,21 +12,21 @@ export function CompareTable<T extends { name: string }>({
   fields: _fields,
 }: CompareTableProps<T>) {
   const colClass =
-    "grid grid-cols-subgrid grid-flow-col grid-cols-4 col-span-5"; // `col-span-5 grid-rows-subgrid grid grid-flow-col `;
+    "grid grid-cols-subgrid grid-flow-col grid-cols-4 col-span-5 border-b-2 gap-2"; // `col-span-5 grid-rows-subgrid grid grid-flow-col `;
   const fields = (_fields || []).map((f) =>
     typeof f !== "object" ? { name: f } : f
   );
   return (
     <div
-      className={`grid grid-flow-row auto-cols-auto auto-rows-auto grid-cols-${
+      className={`grid grid-flow-row gap-3 auto-cols-auto auto-rows-auto grid-cols-${
         sources.length + 1
       } grid-rows-${fields.length + 1}`}
     >
       {fields?.map((field, i) => (
         <div key={i} className={colClass}>
-          <div className="">{field.name.toString()}</div>
+          <div className="uppercase border-r-4">{field.name.toString()}</div>
           {sources.map((src, j) => (
-            <div className="" key={j}>
+            <div className="border-r-4 text-center" key={j}>
               {src[field.name] as string}
             </div>
           ))}
