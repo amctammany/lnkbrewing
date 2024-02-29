7ccfba2a1d9c0ba63582b11434a03413dc2bf513
Alex McTammany <apmctammany@gmail.com>
2024-02-29 00:50:30 -0500

    good enough

--- app/ingredients/hops/_components/HopCombination/HopCombination.tsx
+++ app/ingredients/hops/_components/HopCombination/HopCombination.tsx
@@ -1,19 +1,56 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 //import HopPanel from "./HopPanel";
 import { Hop } from "@prisma/client";
 import { Section } from "@/components/Section";
 import { Form } from "@/components/Form/Form";
 import { useForm } from "react-hook-form";
 import ColumnView, { ColumnField } from "@/components/ColumnView/ColumnView";
+import FieldValue from "@/components/ColumnView/FieldValue";
+import { TextField } from "@/components";
 export type HopCombinationProps = {
   hops: Hop[];
   action: any;
 };
 
-const compFields: ColumnField<Hop>[] = [
+const Radio = ({ name, src, state, setState }) => {
+  console.log({ name, src, state });
+  return <input type="radio" name={name} />;
+};
+const SelectField = ({ name, src, state, setState }) => {
+  const isSelected = state.selected === src.id;
+  const value = src[name];
+  const handleSelect: React.ReactEventHandler<HTMLInputElement> = (e) => {
+    console.log(e.currentTarget.value);
+    setState((old) => ({ ...old, selected: src.id }));
+  };
+  return (
+    <input name={name} onClick={handleSelect} value={src.id} type="radio" />
+  );
+};
+
+const EditFieldValue = ({ name, src, state, setState }) => {
+  const value = src[name];
+  return <TextField name={name} value={value} />;
+};
+const EditField = ({ name, src, state, setState }) => {
+  const value = src[name];
+  const isSelected = state.selected === src.id;
+  const Comp = isSelected ? EditFieldValue : FieldValue;
+  return (
+    <Comp
+      name={name}
+      value={value}
+      src={src}
+      state={state}
+      setState={setState}
+    />
+  );
+};
+const compFields: ColumnField<Hop, { selected: number | null }>[] = [
   "name",
-  "country",
+  { name: "selected", Comp: SelectField },
+  { name: "country", Comp: EditField },
   "characteristics",
   "usage",
   "alpha",
@@ -27,11 +64,17 @@ export function HopCombination({ hops, action }: HopCombinationProps) {
     if (!valid) return;
     if (action) action(data);
   };
+  const [state, setState] = useState({ selected: null });
 
   return (
     <Section header="Hop Comparision">
       <Form action={onAction}>
-        <ColumnView sources={hops} fields={compFields} />
+        <ColumnView
+          sources={hops}
+          fields={compFields}
+          state={state}
+          setState={setState}
+        />
       </Form>
     </Section>
   );
--- components/ColumnView/ColumnView.tsx
+++ components/ColumnView/ColumnView.tsx
@@ -5,25 +5,29 @@ export type ColumnField<T, S = {}> =
   | keyof S
   | {
       name: keyof T | keyof S;
+      Comp?: any;
     };
 export type ColumnViewProps<
   T extends { name: string },
   S extends Record<any, any> = {}
 > = {
   sources: T[];
-  state?: S;
+  state: S;
+  setState: React.Dispatch<React.SetStateAction<S>>;
   fields?: ColumnField<T, S>[];
 };
 export function ColumnView<
   T extends { name: string },
   S extends Record<any, any> = {}
->({ sources, fields: _fields, state }: ColumnViewProps<T, S>) {
-  const fields = (_fields || []).map<{ name: keyof T | keyof S }>((f) =>
-    typeof f !== "object" ? { name: f } : f
-  );
+>({ sources, fields: _fields, state, setState }: ColumnViewProps<T, S>) {
+  const fields = (_fields || [])
+    .map<{ name: keyof T | keyof S; Comp?: any }>((f) =>
+      typeof f !== "object" ? { name: f } : f
+    )
+    .map(({ Comp, ...f }) => ({ Comp: Comp ?? FieldValue, ...f }));
   return (
     <div className="grid grid-flow-row w-full">
-      {(fields || []).map((field, i) => (
+      {fields.map(({ Comp, ...field }, i) => (
         <div
           key={i}
           className={`grid grid-cols-${(
@@ -34,7 +38,14 @@ export function ColumnView<
             {field.name.toString()}
           </div>
           {sources.map((src, j) => (
-            <FieldValue value={src[field.name as keyof T]} key={j} />
+            <Comp
+              value={src[field.name as keyof T]}
+              key={j}
+              src={src}
+              name={field.name}
+              setState={setState}
+              state={state}
+            />
           ))}
         </div>
       ))}
