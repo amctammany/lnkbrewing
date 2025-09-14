b74898d2579cf7c6bbd96a82d3af078adada5757
Alex McTammany <apmctammany@gmail.com>
2025-09-14 01:28:07 -0400

    added bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
+import NextBundleAnalyzer from "@next/bundle-analyzer";
 const nextConfig: NextConfig = {
   /* config options here */
+  /**
+  turbopack: {
+    // root: path.join(__dirname, "."),
+    resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".mjs", ".json"],
+  }, */
+  experimental: {
+    //    ppr: "incremental",
+    //   authInterrupts: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = NextBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+export default withBundleAnalyzer(nextConfig);
