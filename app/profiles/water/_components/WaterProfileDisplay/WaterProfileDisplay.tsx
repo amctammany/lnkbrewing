603d1b7377767cb1527d638b425f31e03cb72069
Alex McTammany <apmctammany@gmail.com>
2025-09-14 20:31:33 -0400

    waterprofile display up ish

--- app/profiles/water/_components/WaterProfileDisplay/WaterProfileDisplay.tsx
+++ app/profiles/water/_components/WaterProfileDisplay/WaterProfileDisplay.tsx
@@ -1,6 +1,27 @@
+import { Ca2, Cl, HCO3, Mg2, MgSo4, Na, SO4 } from "@/components/Elements";
+import { Prop } from "@/components/Prop";
+import { Card } from "@/components/ui/card";
 import { WaterProfile } from "@prisma/client";
 import React from "react";
-
+const Box = ({
+  label,
+  value,
+  unit,
+}: {
+  unit?: string | React.ReactNode;
+  label: string | React.ReactNode;
+  value: number;
+}) => (
+  <div className="grid grid-cols-2 font-mono border-2 rounded *:px-4 *:py-3">
+    <span className="text-lg font-bold bg-slate-700 text-gray-300 ">
+      {label}
+    </span>
+    <div className="flexspacing-2 *:my-auto">
+      <span className="pr-2">{value}</span>
+      <span>{unit}</span>
+    </div>
+  </div>
+);
 export type WaterProfileDisplayProps = {
   profile: WaterProfile;
 };
@@ -9,8 +30,19 @@ export default function WaterProfileDisplay({
 }: WaterProfileDisplayProps) {
   return (
     <div>
-      WaterProfileDisplay
-      <div>{JSON.stringify(profile)}</div>
+      <Card className="*:not-last:border-b-2 max-w-2/3 mx-auto">
+        <Prop label="Name" value={profile.name} />
+        <Prop label="Author" value={profile.userId} />
+        <Prop label="Description" value={profile.description} />
+        <div className="grid grid-cols-3 lg:grid-cols-3 gap-2 p-2">
+          <Box label={<Ca2 />} value={profile.calcium} unit="ppm" />
+          <Box label={<Mg2 />} value={profile.magnesium} unit="ppm" />
+          <Box label={<Cl />} value={profile.chloride} unit="ppm" />
+          <Box label={<SO4 />} value={profile.sulfate} unit="ppm" />
+          <Box label={<HCO3 />} value={profile.bicarbonate} unit="ppm" />
+          <Box label={<Na />} value={profile.sodium} unit="ppm" />
+        </div>
+      </Card>
     </div>
   );
 }
