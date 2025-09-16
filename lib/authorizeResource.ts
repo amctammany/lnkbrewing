036adeb7457c987b2d48fba7374c396fa4bc9c12
Alex McTammany <apmctammany@gmail.com>
2025-09-16 01:21:28 -0400

    move loading

--- lib/authorizeResource.ts
+++ lib/authorizeResource.ts
@@ -1,18 +1,13 @@
-import { auth } from "@/auth";
 //import { User } from "next-auth";
 import { forbidden, notFound, redirect, unauthorized } from "next/navigation";
+import { verifySession } from "./verifySession";
 
 export async function authorizeResource<T extends object>(
   redirectTo: string,
   fn: any,
   ...args: any[]
 ) {
-  const session = await auth();
-  if (!session?.user)
-    return redirect(
-      "/admin/login?" +
-        new URLSearchParams({ redirect_url: redirectTo }).toString()
-    );
+  const session = await verifySession(redirectTo);
   //if (session?.role !== "SUPERUSER") return forbidden();
   const resource = await fn(...args);
   if (!resource) return notFound();
