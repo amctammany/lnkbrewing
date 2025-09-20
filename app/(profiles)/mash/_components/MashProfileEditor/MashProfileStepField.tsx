236a049b4d41d85964d5a0fb9751ab9778b1a596
Alex McTammany <apmctammany@gmail.com>
2025-09-20 02:42:02 -0400

    good enough

--- app/(profiles)/mash/_components/MashProfileEditor/MashProfileStepField.tsx
+++ app/(profiles)/mash/_components/MashProfileEditor/MashProfileStepField.tsx
@@ -1,6 +1,8 @@
 import InlineField from "@/components/Form/InlineField";
+import SelectInput from "@/components/Form/SelectInput";
 import { Button } from "@/components/ui/button";
 import { MashProfileType, MashStepType } from "@/types/Profile";
+import { $Enums } from "@prisma/client";
 import React from "react";
 import { Control } from "react-hook-form";
 export type MashProfileStepFieldProps = {
@@ -21,54 +23,68 @@ export default function MashProfileStepField({
 }: MashProfileStepFieldProps) {
   return (
     <li className="list-item" key={index}>
-      <b>{index}</b>
-      <b>{src.type}</b>
-      <input type="hidden" name={`steps.${index}.index`} value={index} />
-      <input type="hidden" name={`steps.${index}.id`} />
-      <InlineField
-        control={control}
-        name={`steps.${index}.temperature`}
-        type="number"
-        label="Temp"
-      />
-      <InlineField
-        control={control}
-        name={`steps.${index}.time`}
-        type="number"
-        label="Time"
-      />
-      <InlineField
-        control={control}
-        name={`steps.${index}.rampTime`}
-        type="number"
-        label="Ramp Time"
-      />
-      <Button
-        onClick={(e) => {
-          e.preventDefault();
-          swap(index, index - 1);
-        }}
-        disabled={index < 1}
-      >
-        Up
-      </Button>
-      <Button
-        onClick={(e) => {
-          e.preventDefault();
-          remove(index);
-        }}
-      >
-        Remove
-      </Button>
-      <Button
-        onClick={(e) => {
-          e.preventDefault();
-          swap(index, index + 1);
-        }}
-        disabled={index > length - 1}
-      >
-        Down
-      </Button>
+      <div className="flex">
+        <div className="grid grid-cols-3">
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              swap(index, index - 1);
+            }}
+            disabled={index < 1}
+          >
+            Up
+          </Button>
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              remove(index);
+            }}
+          >
+            Remove
+          </Button>
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              swap(index, index + 1);
+            }}
+            disabled={index > length - 1}
+          >
+            Down
+          </Button>
+        </div>
+        <div className="flex-grow grid grid-cols-4">
+          <input type="hidden" name={`steps.${index}.index`} value={index} />
+          <input type="hidden" name={`steps.${index}.id`} />
+          <SelectInput
+            control={control}
+            label="Type"
+            variant="inline"
+            name={`steps.${index}.type`}
+            options={$Enums.MashStepType}
+          />
+          <InlineField
+            control={control}
+            name={`steps.${index}.temperature`}
+            variant="inline"
+            type="number"
+            label="Temp"
+          />
+          <InlineField
+            variant="inline"
+            control={control}
+            name={`steps.${index}.time`}
+            type="number"
+            label="Time"
+          />
+          <InlineField
+            control={control}
+            variant="inline"
+            name={`steps.${index}.rampTime`}
+            type="number"
+            label="Ramp Time"
+          />
+        </div>
+      </div>
     </li>
   );
 }
