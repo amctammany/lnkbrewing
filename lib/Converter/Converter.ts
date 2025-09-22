b936a2323ace94e2fbafee617d8cd516cb113504
Alex McTammany <apmctammany@gmail.com>
2025-09-21 23:01:03 -0400

    works

--- lib/Converter/Converter.ts
+++ lib/Converter/Converter.ts
@@ -10,19 +10,20 @@ const massConverter: Record<MassUnit, ConversionType> = {
 
 const tempConverter: Record<UserTemperaturePreference, ConversionType> = {
   C: 1,
-  F: [(f: number) => (f - 32) * (5 / 9), (c: number) => c * (9 / 5) + 32],
+  F: [(c: number) => c * (9 / 5) + 32, (f: number) => (f - 32) * (5 / 9)],
 };
-const converters: Partial<Record<UnitTypes, any>> = {
-  mass: makeConverter(massConverter),
-  temperature: makeConverter(tempConverter),
-};
+const converters: Partial<Record<UnitTypes, Record<UnitNames, ConverterType>>> =
+  {
+    mass: makeConverter(massConverter),
+    temperature: makeConverter(tempConverter),
+  };
 
 export type ConversionType =
   | number
   //  | [number, number]
   | [(x: number) => number, (x: number) => number];
 export type ConverterDict = Partial<Record<UnitNames, ConversionType>>;
-function makeConverter(src: ConverterDict) {
+function makeConvertFn(src: ConverterDict) {
   return (
     value: number,
     from: keyof ConverterDict,
@@ -38,11 +39,30 @@ function makeConverter(src: ConverterDict) {
     return res;
   };
 }
+export type ConverterType = {
+  to: (v: number) => number;
+  from: (v: number) => number;
+};
+function makeConverter(src: ConverterDict) {
+  return Object.entries(src).reduce((acc, [unit, converter]) => {
+    const to = Array.isArray(converter)
+      ? converter[0]
+      : (v: number) => v * converter;
+    const from = Array.isArray(converter)
+      ? converter[1]
+      : (v: number) => v / converter;
+    acc[unit as UnitNames] = { to, from };
+    return acc;
+  }, {} as Record<UnitNames, ConverterType>);
+}
 export function Converter(value: number, from: UnitNames, to: UnitNames) {
   const group = UnitDict[from];
   if (UnitDict[to] !== group)
     throw new Error("Cannot convert between two different measurements");
   const convert = converters[group];
   if (!convert) throw new Error("Converter not available");
-  return convert(value, from, to);
+  const baseValue = convert[from].from(value);
+  const newValue = convert[to].to(baseValue);
+  console.log({ to, from, group, value, baseValue, newValue });
+  return newValue;
 }
